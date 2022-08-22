3a2236fc8d8a26e23c1abb45b15497d395766a27
pradojeric <jericprado0719@gmail.com>
2022-08-22 10:52:20 +0800

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,10 @@
                                 <input type="radio" value="one_way" x-model="trip" id="one_way" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="one_way" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">One Way</label>
                             </div>
-                            <div class="flex items-center mr-4">
+                            {{-- <div class="flex items-center mr-4">
                                 <input type="radio" value="returning" x-model="trip" id="returning" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="returning" value="Returning" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Returning</label>
-                            </div>
+                            </div> --}}
                         </div>
 
                         <div class="grid md:grid-cols-2 md:gap-6">
